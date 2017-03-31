6c80b414ab5d753e99743bde7820894e15cb8824
Balthild Ires <ibalthild@gmail.com>
2017-03-31 18:18:53 +0800

    Fix search form action URL

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" id="searchform" class="searchform" action="http://wordpress/">
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url('/'); ?>">
     <div>
         <label class="screen-reader-text" for="s">搜索：</label>
         <input type="text" value="" name="s" id="s">
